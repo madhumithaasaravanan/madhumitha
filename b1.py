cb7beeb59e12136e063d3181b478f244208bac15
madhumithaasaravanan <49307435+madhumithaasaravanan@users.noreply.github.com>
2019-04-11 12:18:30 +0530

    b1.py

---
+++ b1.py
@@ -0,0 +1,8 @@
+num=int(input("Enter the number:"))
+if num >= 0:
+  if num == 0:
+    print("The given number is zero")
+  else:
+    print("The given number is positive")
+else:
+  print("The given number is negative")
